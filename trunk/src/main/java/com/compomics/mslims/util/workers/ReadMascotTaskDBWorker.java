a1d07121eb432d7e8900672c8a61252d98517ac4
lennart.martens <lennart.martens@bbe1ef78-0a7d-11df-8158-c7cbc0dd6240>
2010-06-23 08:14:59 +0000

    Updated the handling of the default Mascot TaskDB (an MS Access mdb file) since 64 bit  versions of Java can no longer access the 32 bit drivers by Microsoft, and since 64 bit drivers (from Office 10) can't access the 32 bit Task DBs. The 100% Java Jackcess library is now employed, that seemlessly connects to the mdb files regardless of 32 or 64 bit Java (and OS) architecture. While this commit rebrands the tool as 7.4, it is not yet a complete update; QuantificatonGUI needs to be updated still!

--- trunk/src/main/java/com/compomics/mslims/util/workers/ReadMascotTaskDBWorker.java
+++ trunk/src/main/java/com/compomics/mslims/util/workers/ReadMascotTaskDBWorker.java
@@ -6,6 +6,8 @@
  */
 package com.compomics.mslims.util.workers;
 
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.Table;
 import org.apache.log4j.Logger;
 
 import com.compomics.mslims.gui.progressbars.DefaultProgressBar;
@@ -14,8 +16,9 @@ import com.compomics.mslims.gui.tree.MascotTask;
 import com.compomics.util.interfaces.Flamable;
 import com.compomics.util.sun.SwingWorker;
 
+import java.io.IOException;
 import java.sql.*;
-import java.util.Vector;
+import java.util.*;
 
 /*
  * CVS information:
@@ -32,6 +35,17 @@ import java.util.Vector;
 public class ReadMascotTaskDBWorker extends SwingWorker {
     // Class specific log4j logger for ReadMascotTaskDBWorker instances.
     private static Logger logger = Logger.getLogger(ReadMascotTaskDBWorker.class);
+
+    /**
+     * Boolean to indicate whether the connection is to an MS Access file,
+     * or to a relational database.
+     */
+    private boolean iFromDatabase = false;
+
+    /**
+     * Access database object for the Mascot Daemon Task DB.
+     */
+    private Database iTaskDB = null;
 
     /**
      * This is the Connection to the Mascot Task DB.
@@ -54,7 +68,8 @@ public class ReadMascotTaskDBWorker extends SwingWorker {
     private DefaultProgressBar iProgress = null;
 
     /**
-     * This constructor allows the creation and initialization of this Runner. It takes the necessary arguments to
+     * This constructor allows the creation and initialization of this Runner. By specifying a DB connection,
+     * the data will be loaded from a relational database. It takes the necessary arguments to
      * create a workable runner.
      *
      * @param aConn     Connection to the Mascot Task DB.
@@ -67,12 +82,39 @@ public class ReadMascotTaskDBWorker extends SwingWorker {
         iAllTasks = aAllTasks;
         iFlamable = aParent;
         iProgress = aProgress;
+        iFromDatabase = true;
+    }
+
+    /**
+     * This constructor allows the creation and initialization of this Runner. By specifying an MS Access Database object,
+     * the data will be loaded directly from the '.mdb' file. It takes the necessary arguments to
+     * create a workable runner.
+     *
+     * @param aTaskDB   Database with the MS Access file for the Mascot Daemon Task DB.
+     * @param aAllTasks Vector to hold the tasks after completion. Note that this is a reference parameter!
+     * @param aParent   Flamable instance that called this worker.
+     * @param aProgress DefaultProgressBar to show the progress on.
+     */
+    public ReadMascotTaskDBWorker(Database aTaskDB, Vector aAllTasks, Flamable aParent, DefaultProgressBar aProgress) {
+        iTaskDB = aTaskDB;
+        iAllTasks = aAllTasks;
+        iFlamable = aParent;
+        iProgress = aProgress;
+        iFromDatabase = false;
+    }
+
+    public Object construct() {
+        if (iFromDatabase) {
+            return retrieveFromDatabase();
+        } else {
+            return retrieveFromAccessFile();
+        }
     }
 
     /**
      * This method reads all the data from the Mascot Task DB via ODBC (using the Sun JDBC-ODBC bridge driver).
      */
-    public Object construct() {
+    public Object retrieveFromDatabase() {
         try {
             Statement stat = iConn.createStatement();
             PreparedStatement ps =
@@ -135,6 +177,95 @@ public class ReadMascotTaskDBWorker extends SwingWorker {
             logger.error(sqle.getMessage(), sqle);
             iProgress.dispose();
             iFlamable.passHotPotato(sqle);
+        }
+        iProgress.setValue(iProgress.getMaximum());
+        return "";
+    }
+
+    public Object retrieveFromAccessFile() {
+        try {
+            Table table = iTaskDB.getTable("Mascot_Daemon_Results");
+            Iterator<Map<String, Object>> iter = table.iterator();
+            HashMap<Integer, Vector<MascotSearch>> mascotResults = new HashMap<Integer, Vector<MascotSearch>>();
+            // Change the message on the progress bar.
+            iProgress.setMessage("Reading search results...");
+            while (iter.hasNext()) {
+                Map<String, Object> results = iter.next();
+                Integer task_uid = (Integer) results.get("task_UID");
+                String db = (String) results.get("seq_database");
+                String mergefile = (String) results.get("input_data_file");
+                String datfile = (String) results.get("result_url");
+                Timestamp startDate = new Timestamp(((java.util.Date) results.get("submitted")).getTime());
+                Timestamp endDate = new Timestamp(((java.util.Date) results.get("returned")).getTime());
+                String searchTitle = (String) results.get("result_comment");
+                String accession = (String) results.get("accession");
+                // Distiller project can be null!
+                String distiller_project = (String) results.get("distiller_project");
+
+                // First see if there is a datfile.
+                int searchStatus = MascotSearch.STATUS_NORMAL;
+                // If there is none, no identifications were made!
+                if (datfile == null || !(datfile.indexOf(".dat") >= 0) || accession == null || (accession.toLowerCase().indexOf("<no result") >= 0)) {
+                    // This search is skipped.
+                    searchStatus = MascotSearch.STATUS_ERROR;
+                } else if (searchTitle == null) {
+                    searchTitle = "<No title>";
+                    searchStatus = MascotSearch.STATUS_WARNING;
+                }
+
+                MascotSearch lMascotSearch = new MascotSearch(searchStatus, searchTitle, db, mergefile, datfile, startDate, endDate, task_uid);
+
+                if (mascotResults.containsKey(task_uid)) {
+                    // Add result to existing task UID.
+                    Vector temp = mascotResults.get(task_uid);
+                    temp.add(lMascotSearch);
+                } else {
+                    // First occurrence of Task UID. Create new arraylist and insert.
+                    Vector<MascotSearch> temp = new Vector<MascotSearch>();
+                    temp.add(lMascotSearch);
+                    mascotResults.put(task_uid, temp);
+                }
+                iProgress.setValue(iProgress.getValue() + 1);
+            }
+
+            iProgress.setMessage("Reading tasks...");
+
+            // Now get the tasks themselves
+            table = iTaskDB.getTable("Mascot_Daemon_Tasks");
+            iter = table.iterator();
+            TreeSet<MascotTask> ts = new TreeSet<MascotTask>(new Comparator<MascotTask>() {
+                public int compare(MascotTask o1, MascotTask o2) {
+                    Integer i1 = new Integer(o1.getNumber());
+                    Integer i2 = new Integer(o2.getNumber());
+                    return -1*(i1.compareTo(i2));
+                }
+            });
+            while (iter.hasNext()) {
+                Map<String, Object> results = iter.next();
+                Integer task_uid = (Integer) results.get("task_UID");
+                String title = (String) results.get("task_label");
+                String params = (String) results.get("parameter_set");
+                String schedule = (String) results.get("schedule_type");
+                String status = (String) results.get("task_status");
+
+                Vector tempResults = new Vector();
+                if (mascotResults.containsKey(task_uid)) {
+                    tempResults = mascotResults.get(task_uid);
+                }
+                ts.add(new MascotTask(title, params, schedule, status, tempResults, task_uid));
+                iProgress.setValue(iProgress.getValue() + 1);
+            }
+            iProgress.setMessage("Sorting tasks...");
+            Iterator<MascotTask> taskIter = ts.iterator();
+            while (taskIter.hasNext()) {
+                iAllTasks.add(taskIter.next());
+            }
+            iProgress.setValue(iProgress.getValue() + 1);
+            iProgress.setMessage("All data read!");
+        } catch (IOException ioe) {
+            logger.error(ioe.getMessage(), ioe);
+            iProgress.dispose();
+            iFlamable.passHotPotato(ioe);
         }
         iProgress.setValue(iProgress.getMaximum());
         return "";
